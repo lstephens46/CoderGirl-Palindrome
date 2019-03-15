cc3679fbba93171a206ba9f1a6aaa067ed5dfadf
Lauren Stephens <9128505+lstephens46@users.noreply.github.com>
2019-03-15 14:05:30 -0500

    before

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -4,11 +4,27 @@ namespace Palindrome
 {
     public static class Program
     {
+        public string word = "";
         public static void Main()
         {
+            IsPalindrome(nemo);
             Console.ReadLine();
         }
 
+        public static bool IsPalindrome(string word)
+        {
+            char[] tempArray = word.ToCharArray();
+            
+            
+
+            
+        }
+
+
+//        The Palindrome assignment is missing some needed details in the instructions.Here they are:
+//1. The name of the method you create should be IsPalindrome
+//2. The method should take a string as the input.
+//3. The method should return a bool - true if the input is a palindrome and false if it is not.
         // TODO: Create a method that recognizes palindromes.
         // The name of the method you create should be IsPalindrome
         // The method should take a string as the input.
